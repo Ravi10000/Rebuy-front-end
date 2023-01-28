39fc89d1fe6b2fa107dfc6e5b2ba196154d6f7c2
Ravi Sharma <ruravi80@gmail.com>
2023-01-28 14:43:40 +0530

    change rebuy logo to mr.phoneX

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -1,7 +1,7 @@
 import "./logo.styles.scss";
 export default function Logo() {
   return (
-    <h1 className="brand">Re<span>Buy</span>
+    <h1 className="brand">Mr.<span>Phone</span>X
     </h1>
   );
 }
